538a8d0308a2a3302182982e146bf43ddc3891e4
adria.vallejo.7e5 <adria.vallejo.7e5@itb.cat>
2023-05-11 16:22:22 +0200

    calendario

--- src/pages/buyProduct/pages/setDate.jsx
+++ src/pages/buyProduct/pages/setDate.jsx
@@ -1,290 +1,107 @@
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import { Menu, Transition } from '@headlessui/react'
-import { DotsVerticalIcon } from '@heroicons/react/outline'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
-import {
-  add,
-  eachDayOfInterval,
-  endOfMonth,
-  format,
-  getDay,
-  isEqual,
-  isSameDay,
-  isSameMonth,
-  isToday,
-  parse,
-  parseISO,
-  startOfToday,
-} from 'date-fns'
-import { Fragment, useState } from 'react'
+import { TableHead, TableRow, TableCell, TableBody } from "@material-ui/core";
+import { useState } from "react";
+import { Container, Row, Col, Form, Button, ProgressBar, Table } from 'react-bootstrap';
 
-const meetings = [
-  {
-    id: 1,
-    name: 'Leslie Alexander',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-05-11T13:00',
-    endDatetime: '2022-05-11T14:30',
-  },
-  {
-    id: 2,
-    name: 'Michael Foster',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-05-20T09:00',
-    endDatetime: '2022-05-20T11:30',
-  },
-  {
-    id: 3,
-    name: 'Dries Vincent',
-    imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-05-20T17:00',
-    endDatetime: '2022-05-20T18:30',
-  },
-  {
-    id: 4,
-    name: 'Leslie Alexander',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-06-09T13:00',
-    endDatetime: '2022-06-09T14:30',
-  },
-  {
-    id: 5,
-    name: 'Michael Foster',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-05-13T14:00',
-    endDatetime: '2022-05-13T14:30',
-  },
-]
+const WEEKDAYS = ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"];
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export function Calendar() {
-  let today = startOfToday()
-  let [selectedDay, setSelectedDay] = useState(today)
-  let [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
-  let firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
-
-  let days = eachDayOfInterval({
-    start: firstDayCurrentMonth,
-    end: endOfMonth(firstDayCurrentMonth),
-  })
-
-  function previousMonth() {
-    console.log(today);
-    let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 })
-    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
+const chunk = (array, size) => {
+  const chunkedArray = [];
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size);
+    while (chunk.length < size) {
+      chunk.unshift("");
+    }
+    chunkedArray.push(chunk);
   }
+  return chunkedArray;
+};
 
-  function nextMonth() {
-    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
-    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
-  }
 
-  let selectedDayMeetings = meetings.filter((meeting) =>
-    isSameDay(parseISO(meeting.startDatetime), selectedDay)
-  )
+export const Calendar = () => {
+  const today = new Date();
+  const [currentMonth, setCurrentMonth] = useState(today.getMonth());
+  const [currentYear, setCurrentYear] = useState(today.getFullYear());
+  const [selectedDay, setSelectedDay] = useState(today);
 
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+  const firstDayOfWeek = new Date(currentYear, currentMonth, 1).getDay();
+  const days = Array.from({ length: daysInMonth }, (_, index) => index + 1);
+  const daysWithBlanks = [...Array(firstDayOfWeek).fill(null), ...days];
+  
+  // Calcular cuántos días adicionales se necesitan para completar la última semana
+  const remainingDays = (7 - (daysInMonth + firstDayOfWeek) % 7) % 7;
+  const daysWithExtendedBlanks = [...daysWithBlanks, ...Array(remainingDays).fill(null)];
+  
+  const weeks = chunk(daysWithExtendedBlanks, 7);
+
+  const handleNextMonth = () => {
+    if (currentMonth === 11) {
+      setCurrentMonth(0);
+      setCurrentYear(currentYear + 1);
+    } else {
+      setCurrentMonth(currentMonth + 1);
+    }
+  };
+
+  const handlePrevMonth = () => {
+    if (currentMonth === 0) {
+      setCurrentMonth(11);
+      setCurrentYear(currentYear - 1);
+    } else {
+      setCurrentMonth(currentMonth - 1);
+    }
+  };
+
+  const handleDayClick = (day) => {
+    setSelectedDay(day);
+  };
+  
   return (
-    <div className="pt-5">
-      <Container>
-        <Row>
-          <Col>
-            <div className="d-flex align-items-center">
-              <h2 className="flex-grow-1 fw-bold text-gray-900">
-                {format(firstDayCurrentMonth, 'MMMM yyyy')}
-              </h2>
-              <button
-                type="button"
-                onClick={previousMonth}
-                className="btn btn-link text-decoration-none text-gray-400 hover:text-gray-500"
-              >
-                <span className="">Previous month</span>
-                <ChevronLeftIcon className="w-25 h-5" aria-hidden="true" />
-              </button>
-              <button
-                onClick={nextMonth}
-                type="button"
-                className="btn btn-link text-decoration-none text-gray-400 hover:text-gray-500"
-              >
-                <span className="">Next month</span>
-                <ChevronRightIcon className="w-25 h-5" aria-hidden="true" />
-              </button>
-            </div>
-            <Row>
-              <Col>S</Col>
-              <Col>M</Col>
-              <Col>T</Col>
-              <Col>W</Col>
-              <Col>T</Col>
-              <Col>F</Col>
-              <Col>S</Col>
-            </Row>
-            <Row>
-              {days.map((day, dayIdx) => (
-                <Col
-                  key={day.toString()}
-                  className={classNames(
-                    dayIdx === 0 && colStartClasses[getDay(day)],
-                    'pb-2'
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <Button onClick={handlePrevMonth} className="w-25">{"<"}</Button>
+        </Col>
+        <Col>
+          <h2>{`${currentMonth + 1} - ${currentYear}`}</h2>
+        </Col>
+        <Col>
+          <Button onClick={handleNextMonth} className="w-25">{">"}</Button>
+        </Col>
+      </Row>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {WEEKDAYS.map((day) => (
+              <th key={day}>{day}</th>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {weeks.map((week, weekIndex) => (
+            <TableRow key={`week-${weekIndex}`}>
+              {week.map((day, dayIndex) => (
+                <TableCell key={`day-${dayIndex}`}>
+                  {day > 0 && day <= daysInMonth ? (
+                    <>
+                      {day}
+                      {day === 12 && (
+                        <div style={{ backgroundColor: 'red' }} className="w-25">
+                          <div>*</div>
+                        </div>
+                      )}
+                    </>
+                  ) : (
+                    <div className="blank"></div>
                   )}
-                >
-                  <button
-                    type="button"
-                    onClick={() => setSelectedDay(day)}
-                    className={classNames(
-                      isEqual(day, selectedDay) && 'text-white',
-                      !isEqual(day, selectedDay) &&
-                        isToday(day) &&
-                        'text-danger-500',
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        isSameMonth(day, firstDayCurrentMonth) &&
-                        'text-gray-900',
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        !isSameMonth(day, firstDayCurrentMonth) &&
-                        'text-gray-400',
-                      isEqual(day, selectedDay) && isToday(day) && 'bg-red-500',
-                      isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        'bg-gray-900',
-                      !isEqual(day, selectedDay) && 'hover:bg-gray-200',
-                      (isEqual(day, selectedDay) || isToday(day)) &&
-                        'font-weight-bold',
-                      'mx-auto flex h-8 w-8 items-center justify-content-center rounded-circle'
-                    )}
-                  >
-                    <time dateTime={format(day, 'yyyy-MM-dd')}>
-                      {format(day, 'd')}
-                    </time>
-                  </button>
-
-                  <div className="w-1 h-1 mx-auto mt-1">
-                    {meetings.some((meeting) =>
-                      isSameDay(parseISO(meeting.startDatetime), day)
-                    ) && (
-                      <div className="w-1 h-1 rounded-circle bg-sky-500"></div>
-                    )}
-                  </div>
-                </Col>
+                </TableCell>
               ))}
-            </Row>
-          </Col>
-          <section className="col-md-6 mt-5 mt-md-0">
-            <h2 className="fw-bold text-gray-900">
-              Schedule for{' '}
-              <time dateTime={format(selectedDay, 'yyyy-MM-dd')}>
-                {format(selectedDay, 'MMM dd, yyy')}
-              </time>
-            </h2>
-            <Col className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
-              {selectedDayMeetings.length > 0 ? (
-                selectedDayMeetings.map((meeting) => (
-                  <Meeting meeting={meeting} key={meeting.id} />
-                ))
-              ) : (
-                <p>No meetings for today.</p>
-              )}
-            </Col>
-          </section>
-        </Row>
-      </Container>
-    </div>
-  )
-}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+};
 
-function Meeting({ meeting }) {
-  let startDateTime = parseISO(meeting.startDatetime)
-  let endDateTime = parseISO(meeting.endDatetime)
-
-  return (
-    <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
-      <img
-        src={meeting.imageUrl}
-        alt=""
-        className="flex-none w-10 h-10 rounded-full"
-      />
-      <div className="flex-auto">
-        <p className="text-gray-900">{meeting.name}</p>
-        <p className="mt-0.5">
-          <time dateTime={meeting.startDatetime}>
-            {format(startDateTime, 'h:mm a')}
-          </time>{' '}
-          -{' '}
-          <time dateTime={meeting.endDatetime}>
-            {format(endDateTime, 'h:mm a')}
-          </time>
-        </p>
-      </div>
-      <Menu
-        as="div"
-        className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
-      >
-        <div>
-          <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
-            <span className="sr-only">Open options</span>
-            <DotsVerticalIcon className="w-6 h-6" aria-hidden="true" />
-          </Menu.Button>
-        </div>
-
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    Edit
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    Cancel
-                  </a>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </li>
-  )
-}
-
-let colStartClasses = [
-  '',
-  'col-start-2',
-  'col-start-3',
-  'col-start-4',
-  'col-start-5',
-  'col-start-6',
-  'col-start-7',
-]
